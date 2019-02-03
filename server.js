9360c20679dabea8f147d1109d2624f4116f0ecd
Ahmed Mahmoud <ahmed.m.web.dev@gmail.com>
2019-02-03 10:49:26 +0400

    testing server

--- server.js
+++ server.js
@@ -2,3 +2,11 @@
 require("dotenv/config");
 const express = require("express");
 const bodyParser = require("body-parser");
+
+//create the app
+const app = express();
+
+//listen to server
+app.listen(3000, () => {
+    console.log("server is running");
+})
