8768a17ab8e6c0c5c3d8180ac7125a9bb4ab1d32
Ahmed Mahmoud <ahmed.m.web.dev@gmail.com>
2019-02-03 10:54:45 +0400

    server configuration

--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 require("dotenv/config");
 const express = require("express");
 const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL;
 
 //create the app
 const app = express();
 
+//configure app
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //listen to server
-app.listen(3000, () => {
-    console.log("server is running");
-})
+app.listen(PORT, () => {
+    console.log(`Server is running${URL}:${PORT}`);
+});
