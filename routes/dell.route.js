48800b6b8d5ffe963e820e9f8a64f713c817b7db
Ahmed Mahmoud <ahmed.m.web.dev@gmail.com>
2019-02-11 17:39:13 +0400

    get alarms

--- routes/dell.route.js
+++ routes/dell.route.js
@@ -12,5 +12,5 @@ router.post('/save_config', DellController.save_config);
 router.get("/delete/:id", DellController.delete);
 router.get("/edit/:id", DellController.edit);
 router.post("/edit_config", DellController.update);
-router.get(`${dell_config_data.API_PATH}`, DellController.dell_alarm)
+router.post(`${dell_config_data.API_PATH}`, DellController.dell_alarm)
 module.exports = router;
