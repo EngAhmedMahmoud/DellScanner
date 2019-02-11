2537be06ffdd2b1eb6d2a4f58f69a6355c67291e
Ahmed Mahmoud <ahmed.m.web.dev@gmail.com>
2019-02-11 17:37:14 +0400

    init repeater

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const DellRouter = require("./routes/dell.route");
 const fs = require("fs");
 const server_config_file = fs.readFileSync("./config/server.config.json");
 const server_config_data = JSON.parse(server_config_file);
-
+//connection repeater
+const connectionRepeater = require("./utils/connection_repeater");
 const PORT = server_config_data.SERVER_PORT || 3000;
 const URL = server_config_data.SERVER_URL;
 
+//init connection repeater
+connectionRepeater.init;
 //create the app
 const app = express();
 
@@ -27,21 +30,21 @@ app.use(bodyParser.json());
 app.use('', DellRouter);
 
 //handle 404 error
-// app.use((req, res, next) => {
-//     let error = new Error;
-//     if (error) {
-//         error.code = 404;
-//         error.msg = "Not Found";
-//         return next(error);
-//     }
-// });
-// app.use((error, req, res, next) => {
-//     if (error) {
-//         res.render('partials/404');
-//     } else {
-//         next();
-//     }
-// });
+app.use((req, res, next) => {
+    let error = new Error;
+    if (error) {
+        error.code = 404;
+        error.msg = "Not Found";
+        return next(error);
+    }
+});
+app.use((error, req, res, next) => {
+    if (error) {
+        res.render('partials/404');
+    } else {
+        next();
+    }
+});
 
 //listen to server
 app.listen(PORT, () => {
