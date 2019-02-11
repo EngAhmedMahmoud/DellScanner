bdbb6463e95d2cb37e0e2e99907e5471406d4244
Ahmed Mahmoud <ahmed.m.web.dev@gmail.com>
2019-02-11 17:36:42 +0400

    Create checker repeater

--- utils/connection_repeater.js
+++ utils/connection_repeater.js
@@ -1,1 +1,14 @@
 "use strict";
+const Alarm = require("./../models/Alarm");
+
+function RepeaterCheck() {
+    //compare the fack alarm time with the current time
+    Alarm.find().then((devices) => {
+        for (let index = 0; index < devices.length; index++) {
+            console.log(devices[index].created_at);
+        }
+    }).catch(error => { console.log(error) });
+
+}
+//calling function every 
+exports.init = setInterval(RepeaterCheck, 1000 * 60);
